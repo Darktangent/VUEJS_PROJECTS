b543a572d073484ecb0d0ca3f29b82df23598335
Ron Ganguly <c1ph3r9@gmail.com>
2019-02-14 23:55:18 -0600

    add video browser project initial state

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
+    
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import VideoList from './components/VideoList'
+import SearchBar from './components/SearchBar';
+const API_KEY=`AIzaSyCEXyZdll0lkbLHyUrpVZCVUnUY1MDjfXg`;
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name:"App",
+  components:{
+    SearchBar,
+    VideoList
+  },
+  data:function(){
+    return { videos: [] };
+  },
+  methods:{
+    onTermChange(searchTerm){
+      console.log(searchTerm)
+      axios.get('https://www.googleapis.com/youtube/v3/search',{params:{
+        key:API_KEY,
+        type:'video',
+        part:'snippet',
+        q:searchTerm
+      }})
+      .then(response=>{
+        this.videos=response.data.items
+      })
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,30 @@
+<template>
+<ul>
+ <VideoListItem  v-for="video in videos" :video="video" :key="video.etag" >
+
+
+
+ </VideoListItem>
+</ul>
+
+</template>
+
+
+
+<script>
+import VideoListItem from './VideoListItem'
+export default {
+  name:"VideoList",
+  components:{
+    VideoListItem
+  },
+  props:{
+    videos:Array
+    }
+}
+</script>
+
+
+<style>
+
+</style>
